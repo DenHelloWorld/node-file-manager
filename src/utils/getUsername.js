093ddbb8b116313cf16f5d6f803d751d86ef1080
Denis Saveliev <denissaveliev23@gmail.com>
2024-10-11 01:34:09 +0300

    feat: add handle powershell username

--- src/utils/getUsername.js
+++ src/utils/getUsername.js
@@ -1,5 +1,12 @@
 const getUsername = (args) => {
+  const npmUsername = process.env.npm_config_username;
+
+  if (npmUsername) {
+    return npmUsername;
+  }
+
   const usernameArg = args.find((arg) => arg.startsWith('--username='));
   return usernameArg ? usernameArg.split('=')[1] : 'Guest';
 };
+
 export default getUsername;
