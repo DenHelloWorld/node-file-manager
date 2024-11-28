4107ee81084025a3e65d45c309aecf8edd36ba66
Denis Saveliev <denissaveliev23@gmail.com>
2024-11-28 22:31:46 +0200

    refactor: refactor rn add

--- src/commands/rn.js
+++ src/commands/rn.js
@@ -1,16 +1,21 @@
 import { resolve } from 'node:path';
-import { rename } from 'fs/promises';
+import { rename, access, constants } from 'fs/promises';
 import { printSuccess } from '../helpers/printText.js';
 
-const rn = async (path, newFilename) => {
+const rn = async (path, newFileName) => {
   const pathResolwed = resolve(path);
-  const newFilenameResolved = resolve(newFilename);
+  const newFilenameResolved = resolve(newFileName);
 
   try {
-    await rename(pathResolwed, newFilenameResolved);
-    printSuccess(`File renamed from ${path} to ${newFilename}`);
+    await access(newFilenameResolved, constants.F_OK);
+    throw new Error(`A file ${newFileName} already exists in this directory`);
   } catch (error) {
-    throw new Error(error.message);
+    if (error.code === 'ENOENT') {
+      await rename(pathResolwed, newFilenameResolved);
+      printSuccess(`File renamed from ${path} to ${newFileName}`);
+    } else {
+      throw error;
+    }
   }
 };
 export default rn;
