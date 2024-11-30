53034bcf1426bb7a7248c29959c8ba8a23fa06b4
Denis Saveliev <denissaveliev23@gmail.com>
2024-11-30 19:44:07 +0200

    feat: add ability to remove directories

--- src/commands/rm.js
+++ src/commands/rm.js
@@ -1,4 +1,4 @@
-import { unlink } from 'node:fs/promises';
+import { rm as rmFs } from 'node:fs/promises';
 import { resolve } from 'node:path';
 import { printSuccess } from '../helpers/printText.js';
 
@@ -6,7 +6,7 @@ const rm = async (pathToFile) => {
   const resolvedPath = resolve(pathToFile);
 
   try {
-    await unlink(resolvedPath);
+    await rmFs(resolvedPath, { recursive: true });
     printSuccess(`File deleted: ${resolvedPath}`);
   } catch (error) {
     throw new Error(error.message);
